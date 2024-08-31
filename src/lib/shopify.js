19261216e6fdbf56a9ab756267a485a6c86387d3
Arash <76617498+arashghezavati@users.noreply.github.com>
2024-08-31 16:03:36 -0700

    Fix formatting issues in shopify.js

--- src/lib/shopify.js
+++ src/lib/shopify.js
@@ -1,8 +1,8 @@
-import Client from 'shopify-buy';
+import Client from 'shopify-buy'
 
 const client = Client.buildClient({
   domain: 'arashmusicstore.myshopify.com',
   storefrontAccessToken: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
-});
+})
 
-export default client;
+export default client
