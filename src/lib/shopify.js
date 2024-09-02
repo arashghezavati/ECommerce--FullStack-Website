dbb3b89f03af574cbb7130b126430f77666e5800
Arash <76617498+arashghezavati@users.noreply.github.com>
2024-09-02 13:50:50 -0700

    Fix: Correct Prettier formatting issues in shopify.js to ensure successful deployment

--- src/lib/shopify.js
+++ src/lib/shopify.js
@@ -2,7 +2,8 @@ import Client from 'shopify-buy'
 
 const client = Client.buildClient({
   domain: 'arashmusicstore.myshopify.com',
-  storefrontAccessToken: process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+  storefrontAccessToken:
+    process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN,
 })
 
 export default client
