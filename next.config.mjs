51fb1af1d371901b79a787d45da4d9cbd14638ef
Arash <76617498+arashghezavati@users.noreply.github.com>
2024-09-02 14:00:40 -0700

    next.config.mjs  configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.shopify.com'],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 }
 
