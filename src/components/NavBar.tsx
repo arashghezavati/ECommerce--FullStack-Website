8c9d8f52c62f312b25c085470c2c8c7918c8eb9e
Arash <76617498+arashghezavati@users.noreply.github.com>
2024-09-18 18:16:37 -0700

    products is updated

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,12 +3,12 @@ import Link from 'next/link'
 export default function NavBar() {
   return (
     <nav className="flex justify-end space-x-4 p-4 bg-gray-100">
-      <Link href="/categories" className="text-lg font-semibold">
+      {/* <Link href="/categories" className="text-lg font-semibold">
         Categories
       </Link>
       <Link href="/products" className="text-lg font-semibold">
         Products
-      </Link>
+      </Link> */}
       <Link href="/checkout" className="text-lg font-semibold">
         Checkout
       </Link>
