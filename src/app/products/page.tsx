e0db1f9b4b21f55b103aec8f4a4b98e53c213bea
Arash <76617498+arashghezavati@users.noreply.github.com>
2024-09-18 19:51:39 -0700

    products is updated

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,11 @@
 import ProductCard from '@/components/product/ProductCard'
 
 export default async function ProductsPage() {
+  // Fetch products from the API route
   const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/products`, {
-    cache: 'no-store',
+    cache: 'no-store', // Ensures the data is fetched fresh on each request
   })
-  const products = await res.json() 
+  const products = await res.json() // Parse the response data
 
   return (
     <div className="container mx-auto py-8">
